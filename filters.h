5d82baf87f0680e8ded6571b2040e1af711771f2
yuriy <y@ivantsovy.com>
2026-01-12 16:55:47 +0200

    Tidy

--- filters.h
+++ filters.h
@@ -6,6 +6,8 @@
 
 namespace ivantsov
 {
+    using namespace std::numbers;
+
     template<typename T, typename S>
     concept is_algebraic = requires {
         { (((S {} * T {} + S {}) - S {}) + T {}) - T {} } -> std::same_as<S>;
@@ -16,37 +18,30 @@ namespace ivantsov
     namespace Linear::FirstOrder::Details
     {
         enum struct Type { HighPass, LowPass, AllPass, HighShelf, LowShelf };
+        using enum Type;
 
         template<Type Type, std::floating_point T, typename Sample, Warp Warp>
         requires is_algebraic<T, Sample>
         struct StateSpace
         {
-            static constexpr bool is_shelf {Type == Type::HighShelf || Type == Type::LowShelf};
+            static constexpr bool is_shelf {Type == HighShelf || Type == LowShelf};
 
             auto initialize(const auto sample_rate) { fs = sample_rate; }
 
             auto processed(const auto x)
             {
                 const auto theta {(x - z) * b[0]};
-                auto y {theta * b[1]};
-                if constexpr (Type != Type::HighPass) y = y + z;
+                const auto y {Type == HighPass ? theta * b[1] : theta * b[1] + z};
                 z = z + theta;
-                if constexpr (Type == Type::LowShelf) return y * b[2];
-                else return y;
+                return Type == LowShelf ? y * b[2] : y;
             }
 
             auto reset() { z = {}; }
 
             auto set_fc(const auto x)
             {
-                w = fs / (T {2} * std::numbers::pi_v<T> * x);
-                if constexpr (Warp == Warp::Sigma)
-                {
-                    if (w > std::numbers::inv_pi_v<T>)
-                    {
-                        sigma = T {0.408249999896} * (T {0.0584335750974} - w * w) / (T {0.0459329400275} - w * w);
-                    }
-                }
+                w = fs / (T {2} * pi_v<T> * x);
+                if (Warp == Warp::Sigma && w > inv_pi_v<T>) warp_sigma();
                 update_blocks();
             }
 
@@ -58,7 +53,7 @@ namespace ivantsov
 
             auto set_sigma(const auto x) requires(Warp == Warp::None)
             {
-                sigma = x / (T {2} * std::numbers::pi_v<T>);
+                sigma = x / (T {2} * pi_v<T>);
                 update_blocks();
             }
 
@@ -66,65 +61,55 @@ namespace ivantsov
             auto update_blocks()
             {
                 const auto v {[this](const auto x) { return std::sqrt(x + sigma * sigma); }};
-                if constexpr (!is_shelf) b[0] = T {1} / (T {0.5} + v(w * w));
-                if constexpr (Type == Type::HighPass) b[1] = w;
-                if constexpr (Type == Type::LowPass) b[1] = T {0.5} + sigma;
-                if constexpr (Type == Type::AllPass) b[1] = T {0.5} - v(w * w);
-                if constexpr (Type == Type::LowShelf)
-                {
-                    b[0] = T {1} / (T {0.5} + v(w * w * b[2]));
-                    b[1] = T {0.5} + v(w * w / b[2]);
-                }
-                if constexpr (Type == Type::HighShelf)
-                {
-                    b[0] = T {1} / (T {0.5} + v(w * w / b[2]));
-                    b[1] = T {0.5} + v(w * w * b[2]);
-                }
+                b[0] = Type == HighShelf ? T {1} / (T {0.5} + v(w * w / b[2]))
+                     : Type == LowShelf  ? T {1} / (T {0.5} + v(w * w * b[2]))
+                                         : T {1} / (T {0.5} + v(w * w));
+                b[1] = Type == HighShelf ? T {0.5} + v(w * w * b[2])
+                     : Type == LowShelf  ? T {0.5} + v(w * w / b[2])
+                     : Type == AllPass   ? T {0.5} - v(w * w)
+                     : Type == LowPass   ? T {0.5} + sigma
+                                         : w;
             }
 
-            T sigma {std::numbers::inv_pi_v<T>};
+            // scaled approximation of [paper1, eq (2.11)]. Max error: 1%.
+            auto warp_sigma() { sigma = T {0.40824999} * (T {0.05843357} - w * w) / (T {0.04593294} - w * w); }
+
+            T sigma {inv_pi_v<T>};
             T fs {};
             T w {};
             std::array<T, 2 + is_shelf> b {T {}, T {0.5} + sigma};
             Sample z {};
         };
-    }// namespace Linear::FirstOrder::Details
+    } // namespace Linear::FirstOrder::Details
 
     namespace Linear::SecondOrder::Details
     {
         enum struct Type { HighPass, BandPass, LowPass, AllPass, Notch, HighShelf, LowShelf, MidShelf };
+        using enum Type;
 
         template<Type Type, std::floating_point T, typename Sample, Warp Warp>
         requires is_algebraic<T, Sample>
         struct StateSpace
         {
-            static constexpr bool is_shelf {Type == Type::LowShelf || Type == Type::HighShelf ||
-                                            Type == Type::MidShelf};
+            static constexpr auto is_shelf {Type == LowShelf || Type == HighShelf || Type == MidShelf};
 
             auto initialize(const auto sample_rate) { fs = sample_rate; }
 
             auto processed(const auto x)
             {
                 const auto theta {(x - z[0] - z[1] * b[1]) * b[0]};
-                auto y {theta * b[3] + z[1] * b[2]};
-                if constexpr (Type != Type::HighPass && Type != Type::BandPass) y = y + z[0];
+                const auto y {(Type == HighPass || Type == BandPass) ? theta * b[3] + z[1] * b[2]
+                                                                     : theta * b[3] + z[1] * b[2] + z[0]};
                 z = {z[0] + theta, Sample {} - z[1] - theta * b[1]};
-                if constexpr (Type == Type::LowShelf) return y * b[4];
-                else return y;
+                return Type == LowShelf ? y * b[4] : y;
             }
 
             auto reset() { z = {}; }
 
             auto set_fc(const auto x)
             {
-                w = fs / (std::numbers::sqrt2_v<T> * std::numbers::pi_v<T> * x);
-                if constexpr (Warp == Warp::Sigma)
-                {
-                    if (w > std::numbers::inv_pi_v<T> * std::numbers::sqrt2_v<T>)
-                    {
-                        sigma = T {0.577352686692} * (T {0.116867150195} - w * w) / (T {0.091865880055} - w * w);
-                    }
-                }
+                w = fs / (sqrt2_v<T> * pi_v<T> * x);
+                if (Warp == Warp::Sigma && w > inv_pi_v<T> * sqrt2_v<T>) warp_sigma();
                 update_blocks();
             }
 
@@ -142,7 +127,7 @@ namespace ivantsov
 
             auto set_sigma(const auto x) requires(Warp == Warp::None)
             {
-                sigma = x / (std::numbers::sqrt2_v<T> * std::numbers::pi_v<T>);
+                sigma = x / (sqrt2_v<T> * pi_v<T>);
                 update_blocks();
             }
 
@@ -151,95 +136,77 @@ namespace ivantsov
             {
                 const auto w_sq {w * w};
                 const auto sigma_sq {sigma * sigma};
-                const auto l_vk {[&](const auto x, const auto y) {
-                    const auto t {x * (y + y - T {1})};
-                    return std::pair {std::sqrt(x * x + T {2} * t * sigma_sq + sigma_sq * sigma_sq), t + sigma_sq};
+                const auto zeta_sq {zeta * zeta};
+                const auto vk {[sigma_sq](const auto x, const auto y) {
+                    const auto t {x * (y + y) - x};
+                    return std::pair {std::sqrt(x * x + sigma_sq * (t + t + sigma_sq)), t + sigma_sq};
                 }};
-                const auto [v, k] {[&] {
-                    if constexpr (Type == Type::LowShelf) return l_vk(w_sq * std::sqrt(b[4]), zeta * zeta);
-                    if constexpr (Type == Type::HighShelf) return l_vk(w_sq / std::sqrt(b[4]), zeta * zeta);
-                    if constexpr (Type == Type::MidShelf) return l_vk(w_sq, zeta * zeta / b[4]);
-                    else return l_vk(w_sq, zeta * zeta);
-                }()};
+                const auto [v, k] {Type == LowShelf    ? vk(w_sq * std::sqrt(b[4]), zeta_sq)
+                                   : Type == HighShelf ? vk(w_sq / std::sqrt(b[4]), zeta_sq)
+                                   : Type == MidShelf  ? vk(w_sq, zeta_sq / b[4])
+                                                       : vk(w_sq, zeta_sq)};
+                const auto [v_a, k_a] {Type == LowShelf    ? vk(w_sq / std::sqrt(b[4]), zeta_sq)
+                                       : Type == HighShelf ? vk(w_sq * std::sqrt(b[4]), zeta_sq)
+                                                           : vk(w_sq, zeta_sq * b[4])};
                 b[0] = T {1} / (v + std::sqrt(v + k) + T {0.5});
                 b[1] = std::sqrt(v + v);
-                if constexpr (Type == Type::HighPass)
-                {
-                    b[2] = T {2} * w_sq / b[1];
-                    b[3] = w_sq;
-                }
-                if constexpr (Type == Type::BandPass)
-                {
-                    b[2] = T {4} * w * zeta * sigma / b[1];
-                    b[3] = T {2} * w * zeta * (sigma + T {1} / std::numbers::sqrt2_v<T>);
-                }
-                if constexpr (Type == Type::LowPass)
-                {
-                    b[2] = T {2} * sigma_sq / b[1];
-                    b[3] = sigma_sq + std::numbers::sqrt2_v<T> * sigma + T {0.5};
-                }
-                if constexpr (Type == Type::Notch)
-                {
-                    b[2] = T {2} * (w_sq - sigma_sq) / b[1];
-                    b[3] = w_sq - sigma_sq + T {0.5};
-                }
-                if constexpr (Type == Type::AllPass)
-                {
-                    b[2] = b[1];
-                    b[3] = v - std::sqrt(v + k) + T {0.5};
-                }
-                if constexpr (is_shelf)
-                {
-                    const auto [v_b, k_b] {[&] {
-                        if constexpr (Type == Type::LowShelf) return l_vk(w_sq / std::sqrt(b[4]), zeta * zeta);
-                        if constexpr (Type == Type::HighShelf) return l_vk(w_sq * std::sqrt(b[4]), zeta * zeta);
-                        else return l_vk(w_sq, zeta * zeta * b[4]);
-                    }()};
-                    b[2] = T {2} * v_b / b[1];
-                    b[3] = v_b + std::sqrt(v_b + k_b) + T {0.5};
-                }
+                b[2] = Type == HighPass ? T {2} * w_sq / b[1]
+                     : Type == BandPass ? T {4} * w * zeta * sigma / b[1]
+                     : Type == LowPass  ? T {2} * sigma_sq / b[1]
+                     : Type == Notch    ? T {2} * (w_sq - sigma_sq) / b[1]
+                     : Type == AllPass  ? b[1]
+                                        : T {2} * v_a / b[1];
+                b[3] = Type == HighPass ? w_sq
+                     : Type == BandPass ? T {2} * w * zeta * (sigma + T {1} / sqrt2_v<T>)
+                     : Type == LowPass  ? T {0.5} + sigma_sq + sqrt2_v<T> * sigma
+                     : Type == Notch    ? T {0.5} + w_sq - sigma_sq
+                     : Type == AllPass  ? T {0.5} + v - std::sqrt(v + k)
+                                        : T {0.5} + v_a + std::sqrt(v_a + k_a);
             }
+
+            // scaled approximation of [paper1, eq (2.11)]. Max error: 1%.
+            auto warp_sigma() { sigma = T {0.57735268} * (T {0.11686715} - w * w) / (T {0.09186588} - w * w); }
 
             T fs {};
             T w {};
             T zeta {};
-            T sigma {std::numbers::sqrt2_v<T> * std::numbers::inv_pi_v<T>};
+            T sigma {sqrt2_v<T> * inv_pi_v<T>};
             std::array<T, 4 + is_shelf> b {};
             std::array<Sample, 2> z {};
         };
-    }// namespace Linear::SecondOrder::Details
+    } // namespace Linear::SecondOrder::Details
 
     namespace Linear::FirstOrder
     {
         template<typename T, typename Sample = T, Warp Warp = Warp::None>
-        using LowPass = Details::StateSpace<Details::Type::LowPass, T, Sample, Warp>;
+        using LowPass = Details::StateSpace<Details::LowPass, T, Sample, Warp>;
         template<typename T, typename Sample = T, Warp Warp = Warp::None>
-        using HighPass = Details::StateSpace<Details::Type::HighPass, T, Sample, Warp>;
+        using HighPass = Details::StateSpace<Details::HighPass, T, Sample, Warp>;
         template<typename T, typename Sample = T, Warp Warp = Warp::None>
-        using AllPass = Details::StateSpace<Details::Type::AllPass, T, Sample, Warp>;
+        using AllPass = Details::StateSpace<Details::AllPass, T, Sample, Warp>;
         template<typename T, typename Sample = T, Warp Warp = Warp::None>
-        using LowShelf = Details::StateSpace<Details::Type::LowShelf, T, Sample, Warp>;
+        using LowShelf = Details::StateSpace<Details::LowShelf, T, Sample, Warp>;
         template<typename T, typename Sample = T, Warp Warp = Warp::None>
-        using HighShelf = Details::StateSpace<Details::Type::HighShelf, T, Sample, Warp>;
-    }// namespace Linear::FirstOrder
+        using HighShelf = Details::StateSpace<Details::HighShelf, T, Sample, Warp>;
+    } // namespace Linear::FirstOrder
 
     namespace Linear::SecondOrder
     {
         template<typename T, typename Sample = T, Warp Warp = Warp::None>
-        using LowPass = Details::StateSpace<Details::Type::LowPass, T, Sample, Warp>;
+        using LowPass = Details::StateSpace<Details::LowPass, T, Sample, Warp>;
         template<typename T, typename Sample = T, Warp Warp = Warp::None>
-        using BandPass = Details::StateSpace<Details::Type::BandPass, T, Sample, Warp>;
+        using BandPass = Details::StateSpace<Details::BandPass, T, Sample, Warp>;
         template<typename T, typename Sample = T, Warp Warp = Warp::None>
-        using HighPass = Details::StateSpace<Details::Type::HighPass, T, Sample, Warp>;
+        using HighPass = Details::StateSpace<Details::HighPass, T, Sample, Warp>;
         template<typename T, typename Sample = T, Warp Warp = Warp::None>
-        using Notch = Details::StateSpace<Details::Type::Notch, T, Sample, Warp>;
+        using Notch = Details::StateSpace<Details::Notch, T, Sample, Warp>;
         template<typename T, typename Sample = T, Warp Warp = Warp::None>
-        using AllPass = Details::StateSpace<Details::Type::AllPass, T, Sample, Warp>;
+        using AllPass = Details::StateSpace<Details::AllPass, T, Sample, Warp>;
         template<typename T, typename Sample = T, Warp Warp = Warp::None>
-        using LowShelf = Details::StateSpace<Details::Type::LowShelf, T, Sample, Warp>;
+        using LowShelf = Details::StateSpace<Details::LowShelf, T, Sample, Warp>;
         template<typename T, typename Sample = T, Warp Warp = Warp::None>
-        using MidShelf = Details::StateSpace<Details::Type::MidShelf, T, Sample, Warp>;
+        using MidShelf = Details::StateSpace<Details::MidShelf, T, Sample, Warp>;
         template<typename T, typename Sample = T, Warp Warp = Warp::None>
-        using HighShelf = Details::StateSpace<Details::Type::HighShelf, T, Sample, Warp>;
-    }// namespace Linear::SecondOrder
-}// namespace ivantsov
+        using HighShelf = Details::StateSpace<Details::HighShelf, T, Sample, Warp>;
+    } // namespace Linear::SecondOrder
+} // namespace ivantsov
